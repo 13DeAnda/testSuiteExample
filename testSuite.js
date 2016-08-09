cb7936ac8517878f02b57aa324af819dcc93d7fe
13DeAnda <nora.e.de.anda@gmail.com>
2016-08-08 17:30:56 -0700

    Update testSuite.js

--- testSuite.js
+++ testSuite.js
@@ -17,69 +17,69 @@ function subtotal(items) {
 //test suite
 describe('Test Suite for subtotal ', function(){
     it('subtotal([{price:1,count:3}]) === 3?', function(){
-    		var items = [{price:1,count:3}];
+    	var items = [{price:1,count:3}];
         subtotal(items).then(function(value) {
-						value.should.equal(3);
+		value.should.equal(3);
         });
     });
     it('subtotal([{price:2,count:4}]) !== 3?', function(){
-    		var items = [{price:2,count:2}];
+    	var items = [{price:2,count:2}];
         subtotal(items).then(function(value) {
-						value.should.not.equal(3);
+		value.should.not.equal(3);
         });
     }); 
     it('subtotal([{price:2,count:2}]) === 4?', function(){
-    		var items = [{price:2,count:2}];
+    	var items = [{price:2,count:2}];
         subtotal(items).then(function(value) {
-						value.should.equal(4);
+		value.should.equal(4);
         });
     }); 
     it('subtotal([{price:null,count:2}]) === 0?', function(){
-    		var items = [{price:null,count:2}];
+    	var items = [{price:null,count:2}];
         subtotal(items).then(function(value) {
-						value.should.equal(0);
+		value.should.equal(0);
         });
     }); 
     it('subtotal([{price:0,count:null}]) === 0?', function(){
-    		var items = [{price:2,count:null}];
+    	var items = [{price:2,count:null}];
         subtotal(items).then(function(value) {
-						value.should.equal(0);
+		value.should.equal(0);
         });
     }); 
     it('subtotal([{price:null,count:null}]) === 0?', function(){
-    		var items = [{price:null,count:null}];
+    	var items = [{price:null,count:null}];
         subtotal(items).then(function(value) {
-						value.should.equal(0);
+		value.should.equal(0);
         });
     }); 
     it('subtotal([{price:0,count:2}]) === 0?', function(){
-    		var items = [{price:0,count:2}];
+    	var items = [{price:0,count:2}];
         subtotal(items).then(function(value) {
-						value.should.equal(0);
+		value.should.equal(0);
         });
     }); 
     it('subtotal([{price:2,count:0}]) === 0?', function(){
-    		var items = [{price:2,count:0}];
+    	var items = [{price:2,count:0}];
         subtotal(items).then(function(value) {
-						value.should.equal(0);
+		value.should.equal(0);
         });
     }); 
     it('subtotal([{price:0,count:0}]) === 0?', function(){
-    		var items = [{price:0,count:0}];
+    	var items = [{price:0,count:0}];
         subtotal(items).then(function(value) {
-						value.should.equal(0);
+		value.should.equal(0);
         });
     }); 
     it('fails when passing a string?', function(){
-    		var items = [{price:"hgfgh",count:0}];
+    	var items = [{price:"hgfgh",count:0}];
         subtotal(items).then(function(value) {
-						value.should.equal(NaN);
+		value.should.equal(NaN);
         });
     });
     it('fails when passing undefined?', function(){
-    		var items = [{price:undefined,count:0}];
+    	var items = [{price:undefined,count:0}];
         subtotal(items).then(function(value) {
-						value.should.equal(NaN);
+		value.should.equal(NaN);
         });
     });
   });
